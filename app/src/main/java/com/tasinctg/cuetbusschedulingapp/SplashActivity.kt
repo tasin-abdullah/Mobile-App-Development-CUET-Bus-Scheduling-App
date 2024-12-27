55b29b0595ded16a9cd959eaede54e6eb9fd2e5c
Tasin <officialtasinkhan@gmail.com>
2024-12-27 18:01:08 +0600

    Material Design Added

--- app/src/main/java/com/tasinctg/cuetbusschedulingapp/SplashActivity.kt
+++ app/src/main/java/com/tasinctg/cuetbusschedulingapp/SplashActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -11,9 +13,16 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val fadeInText = findViewById<TextView>(R.id.splashText)
+
+        val fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+
+        fadeInText.startAnimation(fadeInAnimation)
+
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            finish()
         }, 3000) // 3-second delay
     }
 }
